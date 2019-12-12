29db45889add3539d87b5cdbcdae4b335d032570
github username <github email address>
2019-12-12 11:29:36 -0500

    frontend template

---
+++ backend_spec_garage/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    before_action :require_login, except: [:create]
+
+  def show
+    user = User.find(params.require(:id))
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: { token: token(user.id), user_id: user.id  }
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :bio, :avatar, :email, :location)
+  end
+end
